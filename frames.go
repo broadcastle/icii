f76239654b0464962ff36029ac49d0e2fea224b3
Hector Carrizales <regenttrack@gmail.com>
2018-03-23 09:04:02 +0000

    trying to rewrite frames.go

--- frames.go
+++ frames.go
@@ -3,7 +3,9 @@ package icii
 //// Taken from github.com/stunndard/goicy
 
 import (
+	"errors"
 	"io"
+	"log"
 	"os"
 )
 
@@ -94,94 +96,53 @@ func getFrames(f os.File, framesToRead int) (buf []byte, err error) {
 }
 
 func getFrameSize(header []byte) int {
-	var sr, bitrate uint32
-	var res int
 
-	brtable := [...]uint32{
-		0, 32, 64, 96, 128, 160, 192, 224, 256, 288, 320, 352, 384, 416, 448, 0,
-		0, 32, 48, 56, 64, 80, 96, 112, 128, 160, 192, 224, 256, 320, 384, 0,
-		0, 32, 40, 48, 56, 64, 80, 96, 112, 128, 160, 192, 224, 256, 320, 0,
-		0, 32, 48, 56, 64, 80, 96, 112, 128, 144, 160, 176, 192, 224, 256, 0,
-		0, 8, 16, 24, 32, 40, 48, 56, 64, 80, 96, 112, 128, 144, 160, 0}
-	srtable := [...]uint32{
-		44100, 48000, 32000, 0, // mpeg1
-		22050, 24000, 16000, 0, // mpeg2
-		11025, 12000, 8000, 0} // mpeg2.5
-
-	// get and check the mpeg version
 	mpegver := byte((header[1] & 0x18) >> 3)
 	if mpegver == 1 || mpegver > 3 {
 		return 0
 	}
 
-	// get and check mpeg layer
 	layer := byte((header[1] & 0x06) >> 1)
 	if layer == 0 || layer > 3 {
 		return 0
 	}
-
-	brindex := byte((header[2] & 0x0F0) >> 4)
-
-	if mpegver == 3 && layer == 3 {
-		// mpeg1, layer1
-		bitrate = brtable[brindex]
-	}
-	if mpegver == 3 && layer == 2 {
-		// mpeg1, layer2
-		bitrate = brtable[brindex+16]
-	}
-	if mpegver == 3 && layer == 1 {
-		// mpeg1, layer3
-		bitrate = brtable[brindex+32]
-	}
-	if (mpegver == 2 || mpegver == 0) && layer == 3 {
-		// mpeg2, 2.5, layer1
-		bitrate = brtable[brindex+48]
-	}
-	if (mpegver == 2 || mpegver == 0) && (layer == 2 || layer == 1) {
-		//mpeg2, layer2 or layer3
-		bitrate = brtable[brindex+64]
-	}
-	bitrate = bitrate * 1000
-	padding := int(header[2]&0x02) >> 1
-
-	// get and check the 'sampling_rate_index':
 	srindex := byte((header[2] & 0x0C) >> 2)
 	if srindex >= 3 {
 		return 0
 	}
-	if mpegver == 3 {
-		sr = srtable[srindex]
-	}
-	if mpegver == 2 {
-		sr = srtable[srindex+4]
-	}
-	if mpegver == 0 {
-		sr = srtable[srindex+8]
+
+	padding := int(header[2]&0x02) >> 1
+	brindex := byte((header[2] & 0x0F0) >> 4)
+
+	f := frame{
+		mpeg:    mpegver,
+		layer:   layer,
+		sri:     srindex,
+		bri:     brindex,
+		padding: padding,
 	}
 
-	switch mpegver {
-	case 3: // mpeg1
-		if layer == 3 { // layer1
-			res = (int(12*bitrate/sr) * 4) + (padding * 4)
-		}
-		if layer == 2 || layer == 1 {
-			// layer 2 & 3
-			res = int(144*bitrate/sr) + padding
-		}
-
-	case 2, 0: //mpeg2, mpeg2.5
-		if layer == 3 { // layer1
-			res = (int(12*bitrate/sr) * 4) + (padding * 4)
-		}
-		if layer == 2 { // layer2
-			res = int(144*bitrate/sr) + padding
-		}
-		if layer == 1 { // layer3
-			res = int(72*bitrate/sr) + padding
-		}
+	if err := f.findVersion(); err != nil {
+		log.Println(err)
+		return 0
 	}
-	return res
+
+	if err := f.findBitrate(); err != nil {
+		log.Println(err)
+		return 0
+	}
+
+	if err := f.findSampleRate(); err != nil {
+		log.Println(err)
+		return 0
+	}
+
+	if err := f.calculateSize(); err != nil {
+		log.Println(err)
+		return 0
+	}
+
+	return f.size
 }
 
 func isValidFrameHeader(header []byte) (int, bool) {
@@ -215,4 +176,116 @@ func isValidFrameHeader(header []byte) (int, bool) {
 	}
 
 	return 0, true
+}
+
+type frame struct {
+	br      uint32 // bitrate
+	bri     byte   // bitrate index
+	layer   byte   // layer version
+	mpeg    byte   // mpeg version
+	size    int    // frame size
+	sr      uint32 // sample rate
+	sri     byte   // sample rate index
+	padding int    // padding
+	version int    // used to determine mpeg & layer combo
+}
+
+func (f *frame) findVersion() error {
+
+	x := 0
+
+	if f.layer > 3 || f.layer < 1 || f.mpeg > 3 || f.mpeg == 1 {
+		return errors.New("could not find version, missing mpeg and layer info")
+	}
+
+	switch f.mpeg {
+	case 3:
+		x = 2
+	case 2:
+		x = 1
+	default:
+		x = 0
+	}
+
+	f.version = (x * 3) + int(f.layer)
+
+	return nil
+
+}
+
+func (f *frame) findBitrate() error {
+
+	var b byte
+
+	brtable := [...]uint32{
+		0, 32, 64, 96, 128, 160, 192, 224, 256, 288, 320, 352, 384, 416, 448, 0,
+		0, 32, 48, 56, 64, 80, 96, 112, 128, 160, 192, 224, 256, 320, 384, 0,
+		0, 32, 40, 48, 56, 64, 80, 96, 112, 128, 160, 192, 224, 256, 320, 0,
+		0, 32, 48, 56, 64, 80, 96, 112, 128, 144, 160, 176, 192, 224, 256, 0,
+		0, 8, 16, 24, 32, 40, 48, 56, 64, 80, 96, 112, 128, 144, 160, 0}
+
+	switch f.version {
+	case 9:
+		b = 0
+	case 8:
+		b = 16
+	case 7:
+		b = 32
+	case 6, 3:
+		b = 48
+	case 5, 4, 2, 1:
+		b = 64
+	default:
+		return errors.New("unable to find bitrate")
+	}
+
+	f.br = brtable[f.bri+b] * 1000
+
+	return nil
+
+}
+
+func (f *frame) findSampleRate() error {
+
+	srtable := [...]uint32{
+		44100, 48000, 32000, 0, // mpeg1
+		22050, 24000, 16000, 0, // mpeg2
+		11025, 12000, 8000, 0} // mpeg2.5
+
+	var n byte
+
+	switch f.version {
+	case 9, 8, 7: // mpeg 0 | layers 1,2,3
+		n = 0
+	case 6, 5, 4: // mpeg 2 | layers 1,2,3
+		n = 4
+	case 3, 2, 1: // mpeg 3 | layers 1,2,3
+		n = 8
+	default:
+		return errors.New("could not find sample rate")
+	}
+
+	f.sr = srtable[f.sri+n]
+	return nil
+
+}
+
+func (f *frame) calculateSize() error {
+
+	switch f.version {
+	case 9:
+		f.size = (int(12*f.br/f.sr) * 4) + (f.padding * 4)
+	case 8, 7:
+		f.size = int(144*f.br/f.sr) + f.padding
+	case 6, 3:
+		f.size = (int(12*f.br/f.sr) * 4) + (f.padding * 4)
+	case 5, 2:
+		f.size = int(144*f.br/f.sr) + f.padding
+	case 4, 1:
+		f.size = int(72*f.br/f.sr) + f.padding
+	default:
+		return errors.New("unable to calculate size")
+	}
+
+	return nil
 }
