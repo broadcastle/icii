af74e6a2aef4311c22ac08c8836356d971870072
Hector Carrizales <regenttrack@gmail.com>
2018-04-24 07:06:41 +0000

    added comments and fields

--- pkg/database/database.go
+++ pkg/database/database.go
@@ -7,39 +7,58 @@ import (
 // Track is the information needed to store a audio file.
 type Track struct {
 	gorm.Model
-	Album     string  `json:"album"`
-	Artist    string  `json:"artist"`
-	Location  string  `json:"location"`
-	Title     string  `json:"title"`
-	StationID uint    `json:"organization_id"`
-	UserID    uint    `json:"uploader"`
-	Year      string  `json:"year"`
+
+	// Necesary information.
+	Album  string `json:"album"`
+	Artist string `json:"artist"`
+	Title  string `json:"title"`
+	Year   string `json:"year"`
+
+	// Optional information.
+	Genre string `json:"genre"`
+	Tags  []Tag  `json:"tags"`
+
+	// Calculated information.
 	Length    float64 `json:"length"`
-	Genre     string  `json:"genre"`
-	Tags      []Tag   `json:"tags"`
+	Location  string  `json:"location"`
+	StationID uint    `json:"organization_id"`
+	Tempo     int     `json:"tempo"`
+	UserID    uint    `json:"uploader"`
 }
 
-// Tag is for the tags that would be assigned to a audio file.
+// Tag holds information that can be assigned to multiple tracks within a station.
 type Tag struct {
 	gorm.Model
-	Text    string `json:"text"`
-	TrackID uint   `json:"audio_id"`
+
+	// Information that is visible to the station users.
+	Text      string `json:"text"`
+	TrackID   uint   `json:"audio_id"`
+	StationID uint   `json:"station_id"`
 }
 
 // User is the information about the user for icii.
 type User struct {
 	gorm.Model
-	Name     string     `json:"name"`
-	Email    string     `json:"email"`
-	Password string     `json:"password"`
+
+	// Only the user can change this information.
+	Name     string `json:"name"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+
+	// Relationship between the users and the stations.
 	Stations []*Station `gorm:"many2many:user_stations;" json:"organizations"`
 }
 
 // A Station holds the audio tracks and users.
 type Station struct {
 	gorm.Model
-	Name  string  `json:"name"`
-	Slug  string  `json:"slug"`
+
+	// User filled in information.
+	Name   string `json:"name"`
+	Slug   string `json:"slug"`
+	Public bool   `json:"public"`
+
+	// Relationships between users and tracks.
 	Users []*User `gorm:"many2many:user_stations;" json:"users"`
 	Track []Track `json:"audio"`
 }
@@ -47,24 +66,29 @@ type Station struct {
 // UserPermission keeps track of what permission are allowed for a user.
 type UserPermission struct {
 	gorm.Model
+
 	UserID    uint `json:"user_id"`
 	StationID uint `json:"station_id"`
 
-	TrackAdd    bool `json:"track_add"`
-	TrackEdit   bool `json:"track_edit"`
-	TrackRemove bool `json:"track_remove"`
+	// Station track permisions.
+	TrackAdd    bool `json:"track_add"`    // Add tracks to the station.
+	TrackEdit   bool `json:"track_edit"`   // Edit a stations tracks.
+	TrackRemove bool `json:"track_remove"` // Remove a stations tracks.
 
-	UserAdd    bool `json:"user_add"`
-	UserEdit   bool `json:"user_edit"`
-	UserRemove bool `json:"user_remove"`
+	// Station user permissions.
+	UserAdd    bool `json:"user_add"`    // Add users to the station.
+	UserEdit   bool `json:"user_edit"`   // Edit users permissions in the station.
+	UserRemove bool `json:"user_remove"` // Remove user from a station.
 
-	StreamAdd    bool `json:"stream_add"`
-	StreamEdit   bool `json:"stream_edit"`
-	StreamRemove bool `json:"stream_remove"`
+	// Station stream permissions.
+	StreamAdd    bool `json:"stream_add"`    // Create a stream.
+	StreamEdit   bool `json:"stream_edit"`   // Edit a stream.
+	StreamRemove bool `json:"stream_remove"` // Remove a stream.
 
-	StationAdd    bool `json:"station_add"`
-	StationEdit   bool `json:"station_edit"`
-	StationRemove bool `json:"station_remove"`
+	// Station permissions.
+	StationAdd    bool `json:"station_add"`    // Create a station.
+	StationEdit   bool `json:"station_edit"`   // Edit a station.
+	StationRemove bool `json:"station_remove"` // Remove a station.
 }
 
 // Initialize the database tables.
@@ -72,6 +96,6 @@ func initIciiTables(d *gorm.DB) {
 	d.AutoMigrate(&Station{})
 	d.AutoMigrate(&Tag{})
 	d.AutoMigrate(&Track{})
-	d.AutoMigrate(&User{})
 	d.AutoMigrate(&UserPermission{})
+	d.AutoMigrate(&User{})
 }
